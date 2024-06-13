e13d710e9ed7e1313b3e0f82f026b7ac29862832
molki <robertbarcelay@gmail.com>
2024-06-13 14:37:48 -0400

    chqanges

--- time.js
+++
@@ -1,8 +0,0 @@
-var time = document.getElementById('time');
-
-window.onload = displayClock();
-function displayClock(){
-  var display = new Date().toLocaleTimeString();
-  document.body.innerHTML = display;
-  setTimeout(displayClock, 1000); 
-}
