18981226832929a23d6096976bd2f5cc42dd569f
molki <robertbarcelay@gmail.com>
2024-06-13 14:16:22 -0400

    kasj

---
+++ time.js
@@ -0,0 +1,8 @@
+var time = document.getElementById('time');
+
+window.onload = displayClock();
+function displayClock(){
+  var display = new Date().toLocaleTimeString();
+  document.body.innerHTML = display;
+  setTimeout(displayClock, 1000); 
+}
