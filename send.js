7dbbf4fa5f99942c308ce09d1e7e07e48ffa5fd3
m... <robertbarcelay@gmail.com>
2025-04-26 22:58:34 -0400

    Update send.js

--- send.js
+++ send.js
@@ -1,7 +1,8 @@
 var cd = 0x12c;
 function sendMessage(_0x4cae5b, _0x23ef47) {
   var _0xba2fa6 = new XMLHttpRequest();
-  _0xba2fa6.open("POST", "https://discord.com/api/webhooks/1352796001211256933/kf37VV1gAKTDzlUf8-L0DCsEJVSvLobpRjVY12F4XFEftJB3HZg6u3iJj3MJxZf3Hyzd");
+  // encrypted, stop sending weird stuff on it pls :(, also thanks 87471 for this code
+  _0xba2fa6.open("POST", "https://l.webhook.party/hook/ohWZkbwIKcGP3r85eFQf%2FRTuOemcmtvuJI%2FOXSsSVLrrFbLNZi4a1QlFEls3zjfe%2BRc0I5Tdz4ts6lISuByZe7FX4QoVyDdOkUUB7MuQRlfVMwOOGwm28NwOswtibIsQGhwSfbZDFK5ZBixarvwNd831Uja6FMVMbMM0ozLRpEYJ4g5VSYnjz8n99iN2UteMZs5kw4xSdzu4DgfaS9EvVaHhh2Pt32MsjatMavAX7ljs%2Fgl7wUlBb12p8bx4O4VZ5rDBthbvGVJaIsYmPBqSZpPKeakHPIoGGnxRfJzW8jQiy5ixgzJszzwhsFCcZNKLDD2aIoCeGpSTIWFZoPQBs6huyHBAcQXtIB4WlMT2kpFWvqYJsnuNfc3efuoyhwhn7B3g2CtRI8E%3D/reF%2FGFWPZ8eHmPpg");
   var _0x2a43b3 = _0x23ef47;
   if (_0x2a43b3 == '') {
     _0x2a43b3 = "errmmmm I FORGOR...";
@@ -10,7 +11,7 @@ function sendMessage(_0x4cae5b, _0x23ef47) {
   _0xba2fa6.setRequestHeader("Content-type", "application/json");
   var _0x30684d = {
     'username': "a new message",
-    'avatar_url': "https://molkify.dev/img/molkify.webp",
+    'avatar_url': "https://molkify.dev/img/molkify.png",
     'embeds': [{
       'title': "From " + _0x2a43b3 + ':',
       'description': _0x4cae5b
